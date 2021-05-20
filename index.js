61f3bd26786ec13e594d7c1a54baab172ba73baf
Graciliana <eagraciliana@gmail.com>
2021-05-20 19:00:55 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const configExpress = require('./config/configExpress');
+const conexao = require('./infra/conexao');
+const Tabelas = require('./infra/Tabelas')
+
+conexao.connect(error => {
+    if(error) {
+        throw error;
+    }
+    Tabelas.init(conexao);
+    
+    let app = configExpress();
+    app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
+
+});
+
+
+
+
+
